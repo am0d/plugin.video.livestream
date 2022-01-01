bb5bc50b95e6fdb080748dac6d884662fe2957aa
Damien Schoof <damien.schoof@gmail.com>
2022-01-01 14:57:23 -0500

    - add json schema - store last server response and log when error occurs - fix error when no current live stream for account

--- main.py
+++ main.py
@@ -1,79 +1,84 @@
+
+from kodi_six import xbmc, xbmcvfs, xbmcplugin, xbmcgui, xbmcaddon
 from resources.globals import *
 
 
 
+try:
+
+    params=get_params()
+    url=None
+    name=None
+    mode=None
+    event_id=None
+    owner_id=None
+    video_id=None
+    icon = None
+    cat_info = None
+
+    if 'url' in params:
+        url=urllib.unquote_plus(params["url"])
+
+    if 'name' in params:
+        name=urllib.unquote_plus(params["name"])
+
+    if 'mode' in params:
+        mode=int(params["mode"])
+
+    if 'event_id' in params:
+        event_id=urllib.unquote_plus(params["event_id"])
+
+    if 'owner_id' in params:
+        owner_id=urllib.unquote_plus(params["owner_id"])
+
+    if 'icon' in params:
+        icon=urllib.unquote_plus(params["icon"])
+
+    if 'video_id' in params:
+        video_id=urllib.unquote_plus(params["video_id"])
+
+    if 'cat_info' in params:
+        cat_info=urllib.unquote_plus(params["cat_info"])
 
 
-params=get_params()
-url=None
-name=None
-mode=None
-event_id=None
-owner_id=None
-video_id=None
-icon = None
-cat_info = None
+    if mode==None or url==None or len(url)<1:    
+        categories()          
 
-if 'url' in params:
-    url=urllib.unquote_plus(params["url"])
+    elif mode==100:                
+        getCategories()
 
-if 'name' in params:
-    name=urllib.unquote_plus(params["name"])
+    elif mode==101:        
+        getStream(owner_id,event_id,video_id)
 
-if 'mode' in params:
-    mode=int(params["mode"])
+    elif mode==102:
+        #searchLive()
+        search()
 
-if 'event_id' in params:
-    event_id=urllib.unquote_plus(params["event_id"])
+    elif mode==104:
+        getStream(owner_id,event_id,video_id)
 
-if 'owner_id' in params:
-    owner_id=urllib.unquote_plus(params["owner_id"])
+    elif mode==105:
+        getAccountStreams(owner_id)
 
-if 'icon' in params:
-    icon=urllib.unquote_plus(params["icon"])
+    elif mode==106:
+        getCategoryEvents(cat_info)
 
-if 'video_id' in params:
-    video_id=urllib.unquote_plus(params["video_id"])
+    elif mode==107:
+        getHistory()
 
-if 'cat_info' in params:
-    cat_info=urllib.unquote_plus(params["cat_info"])
+    elif mode==150:
+        login()
 
+    elif mode==160:
+        manualEvent()
 
-if mode==None or url==None or len(url)<1:    
-    categories()          
+    elif mode==999:
+        sys.exit()
 
-elif mode==100:                
-    getCategories()
-
-elif mode==101:        
-    getStream(owner_id,event_id,video_id)
-
-elif mode==102:
-    #searchLive()
-    search()
-
-elif mode==104:
-    getStream(owner_id,event_id,video_id)
-
-elif mode==105:
-    getAccountStreams(owner_id)
-
-elif mode==106:
-    getCategoryEvents(cat_info)
-
-elif mode==107:
-    getHistory()
-
-elif mode==150:
-    login()
-
-elif mode==160:
-    manualEvent()
-
-elif mode==999:
-    sys.exit()
-
-if mode == 100:
-    xbmcplugin.endOfDirectory(addon_handle, cacheToDisc=False)
-else:
-    xbmcplugin.endOfDirectory(addon_handle)
+    if mode == 100:
+        xbmcplugin.endOfDirectory(addon_handle, cacheToDisc=False)
+    else:
+        xbmcplugin.endOfDirectory(addon_handle)
+except Error as e:
+    xbmc.log(f"plugin.video.livestream error: {e}", xbmc.LOGERROR)
+    xbmc.log(f"plugin.video.livestream last server response: {last_server_response}", xbmc.LOGERROR)
